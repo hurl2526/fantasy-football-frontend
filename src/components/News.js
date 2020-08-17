ecb162303c23377de9329e9354cdc8d99796ea49
Patrick <patrick.hurley@codeimmersives.com>
2020-08-17 10:45:59 -0400

    started on news articles

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Article from './Article'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -7,11 +8,44 @@ let styles = {
     width: '70%',
   };
   
+
+  
+  componentDidMount() {
+    setTimeout(() => {
+      const newList = [];
+      fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+        .then((data) => data.json())
+        .then((data) => {
+          for (let i = 0; i < 20; i++) {
+            newList.push({
+              title: data[i].title,
+              url: data[i].url,
+              image: data[i].thumbnailUrl,
+            });
+          }
+          this.setState({
+            list: newList,
+          });
+        });
+    }, 3000);
+  }
+  
 function News() {
   return (
 	<div style={styles}>
 		<Carousel >
-			<div >
+    {this.state.list.map((item, idx) => {
+                return (
+                  <div key={idx}>
+                    <img src={item.picture} alt='...'/>
+                    <p className="legend">{item.header}</p>
+                  </div>
+                );
+              })}
+      {/* <div>
+        <Article />
+      </div> */}
+			{/* <div >
 				<img  src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
 				<p className="legend">Hong Kong</p>
 			</div>
@@ -26,7 +60,7 @@ function News() {
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
 				<p className="legend">Las Vegas</p>
-			</div>
+			</div> */}
 		</Carousel>
 	</div>
   );
